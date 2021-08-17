197b6e068236e9d172e1fd6aaf46ab36010d1a7a
Rivaldi <cbot59@gmail.com>
2021-08-17 16:28:17 +0800

    fix: eslint warning

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,7 @@
-const mapDBToModel = ({ id, title, body, tags, created_at, updated_at }) => ({
+const mapDBToModel = ({
+  // eslint-disable-next-line camelcase
+  id, title, body, tags, created_at, updated_at,
+}) => ({
   id,
   title,
   body,
