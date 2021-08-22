e208ba21ff99afed8dbf54308896eec636a60c59
Rivaldi <cbot59@gmail.com>
2021-08-23 00:42:13 +0800

    feat: membuat fungsi verifyRefreshToken

--- src/tokenize/TokenManager.js
+++ src/tokenize/TokenManager.js
@@ -1,10 +1,23 @@
 const Jwt = require('@hapi/jwt');
+const InvariantError = require('../exceptions/InvariantError');
 
 const TokenManager = {
   generateAccessToken: (payload) =>
     Jwt.token.generate(payload, process.env.ACCESS_TOKEN_KEY),
-    generateRefreshToken: (payload) =>
+  generateRefreshToken: (payload) =>
     Jwt.token.generate(payload, process.env.REFRESH_TOKEN_KEY),
+  verifyRefreshToken: (refreshToken) => {
+    const artifacts = Jwt.token.decode(refreshToken);
+
+    try {
+      Jwt.token.verifySignature(artifacts, process.env.REFRESH_TOKEN_KEY);
+
+      const { payload } = artifacts.decoded;
+      return payload;
+    } catch (error) {
+      throw new InvariantError('Refresh token tidak valid');
+    }
+  },
 };
 
 module.exports = TokenManager;
